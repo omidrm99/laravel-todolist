ca1911339df260c4329149db67644c3f91eb85b3
omidrm99 <omidrm99@gmail.com>
2024-05-03 13:19:58 +0330

    a layout created

---
+++ resources/views/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title')
+    <h1>{{$task->title}}</h1>
+@endsection
+
+@section('content')
+    <p>{{$task->description}}</p>
+
+    @if($task->long_description)
+        <p>{{$task->long_description}}</p>
+    @endif
+
+    <p>{{$task->created_at}}</p>
+    <p>{{$task->updated_at}}</p>
+@endsection
