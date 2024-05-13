1efb9f0fc06dfb62fd9ab9ac53798cc04bfebd88
omidrm99 <omidrm99@gmail.com>
2024-05-13 21:13:17 +0330

    delete button added

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -13,4 +13,17 @@
 
     <p>{{$task->created_at}}</p>
     <p>{{$task->updated_at}}</p>
+
+
+
+
+
+    <div>
+        <form action="{{route('tasks.destroy' , ['task' => $task->id])}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">  DELETE ! </button>
+        </form>
+    </div>
+
 @endsection
