a9bfc9d269f1eca5a78734995446091093bdd90d
omidrm99 <omidrm99@gmail.com>
2024-05-24 12:40:49 +0330

    TASK CONTROLLER ADDED INDEX, CREATE , EDIT FUNCTIONS TO IT

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::latest()->paginate();
+        return view('index', compact('tasks'));
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function edit(Task $task)
+    {
+        return view('edit', compact('task'));
+    }
 }
